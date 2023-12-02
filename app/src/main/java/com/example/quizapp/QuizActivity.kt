654b88d3bebdf21da58343b8f54d9b5d936d72b4
Hamza <mhz15019@gmail.com>
2023-12-02 23:47:05 +0500

    first commit

---
+++ app/src/main/java/com/example/quizapp/QuizActivity.kt
@@ -0,0 +1,72 @@
+package com.example.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.quizapp.databinding.ActivityQuizBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class QuizActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityQuizBinding
+    private var count:Int=0
+    private var score:Int=0
+    lateinit var list:ArrayList<QuestionModel>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityQuizBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        list=ArrayList<QuestionModel>()
+
+        Firebase.firestore.collection("quiz")
+            .get().addOnSuccessListener {
+                docs->
+                list.clear()
+                for (i in docs.documents){
+                    var questionmodel=i.toObject(QuestionModel::class.java)
+                    list.add(questionmodel!!)
+                }
+                binding.question.setText(list.get(0).question)
+                binding.option1.setText(list.get(0).option1)
+                binding.option2.setText(list.get(0).option2)
+                binding.option3.setText(list.get(0).option3)
+                binding.option4.setText(list.get(0).option4)
+
+            }
+
+        binding.option1.setOnClickListener{
+            nextData(binding.option1.text.toString())
+        }
+        binding.option2.setOnClickListener{
+            nextData(binding.option2.text.toString())
+        }
+        binding.option3.setOnClickListener{
+            nextData(binding.option3.text.toString())
+        }
+        binding.option4.setOnClickListener{
+            nextData(binding.option4.text.toString())
+        }
+
+    }
+
+    private fun nextData(i: String) {
+        if(list.get(count).answer.equals(i))
+            score++
+        count+=1
+        if(count>=list.size){
+            val intent= Intent(this,ScoreActivity::class.java)
+            intent.putExtra("Score",score)
+            startActivity(intent)
+            finish()
+        }
+        else {
+            binding.question.setText(list.get(count).question)
+            binding.option1.setText(list.get(count).option1)
+            binding.option2.setText(list.get(count).option2)
+            binding.option3.setText(list.get(count).option3)
+            binding.option4.setText(list.get(count).option4)
+        }
+    }
+}
